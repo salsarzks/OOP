ddcff407fa30d81fdab2f350663ef86ee27645ba
salsarzks <114727070+salsarzks@users.noreply.github.com>
2024-06-11 10:40:01 +0700

    Add files via upload

---
+++ libraryweb/src/main/java/com/byun/libraryweb/LibrarywebApplication.java
@@ -0,0 +1,13 @@
+package com.byun.libraryweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarywebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarywebApplication.class, args);
+	}
+
+}
