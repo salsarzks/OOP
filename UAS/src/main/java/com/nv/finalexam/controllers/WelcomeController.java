87f21cafdd9e1e15558c770ea7bf4d7b83cc34fe
salsarzks <114727070+salsarzks@users.noreply.github.com>
2024-06-24 09:27:38 +0700

    WelcomeController.java

--- UAS/src/main/java/com/nv/finalexam/controllers/WelcomeController.java
+++ UAS/src/main/java/com/nv/finalexam/controllers/WelcomeController.java
@@ -1,4 +1,4 @@
-package com.nv.finalexam.controllers;
+package com.byun.finalexam.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
